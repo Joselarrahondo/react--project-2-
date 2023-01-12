7cf2069028ecfe55b00e677a5725f9851cbe4377
Joselarrahondo <jlarraho@gmail.com>
2023-01-12 15:58:13 -0600

    completed text color and back ground

---
+++ react--project-2/src/App.js
@@ -0,0 +1,68 @@
+import React, {useState} from "react"
+import ReactDOM from 'react-dom/client';
+import "./app.css"
+
+
+function App() {
+const [backgroundColor, setBackgroundColor] = useState("white")
+const [inputBackgroundColor, setInputBackgroundColor] = useState("white")
+const [inputTextColor, setInputTextColor] = useState("");
+const [textSize, setTextSize] = useState(10)
+const [textColor, setTextColor] = useState("black")
+const [inputText, setInputText] = useState("")
+const [text , setText] = useState("Your Words here")
+
+
+const handleBackgroundChange = (e) => {
+  setInputBackgroundColor(e.target.value);
+};
+
+const handleClick = () => {
+  setBackgroundColor(inputBackgroundColor);
+  setTextColor(inputTextColor)
+  setText(inputText)
+};
+
+const handleTextChange = (e) => {
+  setInputTextColor(e.target.value);
+};
+const handleTextInput = (e) => {
+  setInputText(e.target.value);
+};
+
+
+
+
+  return (
+    <div className="App">
+      <div>
+         <input type="text" placeholder= "Enter a background color" onChange={handleBackgroundChange} />
+         <input type="text" placeholder="Enter text to display" onChange={handleTextInput}/>
+         <input type="text" placeholder="Enter text color" onChange={handleTextChange}/>
+         <button type='submit' onClick={handleClick}>Submit Change</button>
+      </div>
+  <div className="box"
+      style={{
+        height: "70vh",
+        width: "70vw",
+        backgroundColor: backgroundColor,
+        fontSize: textSize
+      }}>
+        <div className="Text" style={{
+        color: textColor,
+      }}>
+        {text}
+        </div>
+    </div>
+       <div>
+        <button type="submit"onClick={()=> {setTextSize(50)}} >Small</button>
+          <button type="submit" onClick={()=> {setTextSize(75)}}>Medium</button>
+          <button type="submit" onClick={()=> {setTextSize(88)}}>Large </button>
+         
+          
+      </div>
+    </div>
+
+)};
+
+export default App;
